3d42e6aea859e24e8f2deffb8547b5da352896c7
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2026-05-02 14:27:35 +0200

    feat: add PATCH method to update usertrip status after adding new connection

--- routes/urls.py
+++ routes/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('cities/', views.get_cities, name='cities'),
 
     path('trips/', views.trips_manager, name='trips-manager'),
-    path('trips/<slug:slug>/', views.get_trip_detail, name='trip-detail'),
+    path('trips/<slug:slug>/', views.trip_detail, name='trip-detail'),
     path('trips/<slug:slug>/connections/', views.get_trip_connections, name='trip-connections-list'),
 
     path('connections/', views.add_connection, name='add_connection'),
--- routes/views.py
+++ routes/views.py
@@ -65,11 +65,83 @@ def trips_manager(request):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
 
-@api_view(['GET'])
-def get_trip_detail(request, slug):
+@api_view(['GET', 'DELETE', 'PATCH'])
+def trip_detail(request, slug):
     trip = get_object_or_404(UserTrip, slug=slug)
-    serializer = UserTripSerializer(trip)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        serializer = UserTripSerializer(trip)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        trip.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PATCH':
+        connections = trip.connections.all()
+
+        if not connections.exists():
+            return Response({"error": "Cannot complete an empty trip."}, status=400)
+
+        trip.start_date = connections.first().departure_date
+        trip.end_date = connections.last().arrival_date
+        
+        visited_cities = [];
+
+        for connection in connections:
+            start_city = connection.starting_stop.place.place_city
+            end_city = connection.destination_stop.place.place_city
+
+            if not start_city in visited_cities:
+                visited_cities.append(start_city)
+            if not end_city in visited_cities:
+                visited_cities.append(end_city)
+
+        unique_countries = set()
+        unique_regions = set()
+        
+        current_population = 0
+
+        for city in visited_cities:
+
+            if city.city_thumbnail_url and current_population < city.city_population:
+                trip.thumbnail_url = city.city_thumbnail_url
+                current_population = city.city_population
+
+            if city.city_country_name not in unique_countries:
+                unique_countries.add(city.city_country_name)
+            if city.city_region_name not in unique_regions:
+                unique_regions.add(city.city_region_name)
+
+        name_elements = []
+
+        if len(visited_cities) <= 2:
+            name_elements = [c.city_name for c in visited_cities]
+        elif len(unique_countries) > 1:
+            name_elements = unique_countries
+        elif len(unique_regions) > 1:
+            name_elements = unique_regions
+        else:
+            name_elements = [c.city_name for c in visited_cities]
+
+        if len(name_elements) > 3:
+            middle_index = len(name_elements) // 2
+            final_name_parts = [
+                name_elements[0], 
+                name_elements[middle_index], 
+                name_elements[-1]
+            ]
+        else:
+            final_name_parts = name_elements
+
+        if len(final_name_parts) == 3:
+            trip.name = f"{final_name_parts[0]}, {final_name_parts[1]} and {final_name_parts[2]}"
+        elif len(final_name_parts) == 2:
+            trip.name = f"{final_name_parts[0]} and {final_name_parts[1]}"
+        else:
+            trip.name = final_name_parts[0]
+        
+        trip.save()
+        serializer = UserTripSerializer(trip)
+        return Response(serializer.data)
 
 # connections
 
