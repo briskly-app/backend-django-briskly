4ab8eb630a771af654e6d2ca72f83c41fd7b6cdd
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2026-05-28 18:43:54 +0200

    feat: add swagger

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenRefreshView
+from drf_spectacular.utils import OpenApiResponse, extend_schema
 
 from apps.accounts.api.serializers import MeSerializer, UsernamePasswordLoginSerializer
 
@@ -11,6 +12,14 @@ from apps.accounts.api.serializers import MeSerializer, UsernamePasswordLoginSer
 class LoginView(APIView):
     permission_classes = [AllowAny]
 
+    @extend_schema(
+        request=UsernamePasswordLoginSerializer,
+        responses={
+            200: OpenApiResponse(
+                description="JWT token pair (access/refresh) + user payload."
+            )
+        },
+    )
     def post(self, request):
         serializer = UsernamePasswordLoginSerializer(
             data=request.data,
@@ -24,6 +33,10 @@ class RefreshView(TokenRefreshView):
     permission_classes = [AllowAny]
 
 
+@extend_schema(
+    request=None,
+    responses={200: MeSerializer},
+)
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def me(request):
